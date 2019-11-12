99f4eb91d4bd8bb59f047b57f0c6c482b8d501ce
xiaofang <xiaofangn007@xf.com>
2019-11-11 18:09:45 -0800

    fix the run_unittest.sh can not run on the command line

--- run_unittest.sh
+++ run_unittest.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 export PYTHONPATH=src:venv/lib/python3.6:venv/lib/python3.6/site-packages
-coverage run --source src -m pytest -vv
-coverage report -m
+venv/bin/coverage run --source src -m pytest -vv
+venv/bin/coverage report -m
